95e06752ccb67f17565648db002faf39d3ed497f
rouol <karrauln@yandex.ru>
2022-02-22 16:41:43 +0300

    first build

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-import sys
+import sys, os
 
 from pybind11 import get_cmake_dir
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pyrsistent import inc
 from setuptools import setup
 
 __version__ = "0.0.1"
@@ -16,21 +17,34 @@ __version__ = "0.0.1"
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
+# These are my includes...
+# note that /clib/include only exists after calling clib_install.sh
+cwd = os.path.dirname(os.path.abspath(__file__))
+include_dirs = [
+    cwd,
+    cwd + '\\src\\include',
+    cwd + '\\src\\include\\fparser',
+    cwd + '\\src\\include\\eigen-3.3.9',
+]
+
+print(include_dirs)
+
 ext_modules = [
-    Pybind11Extension("python_example",
-        ["src/main.cpp"],
+    Pybind11Extension("dyns",
+        ["src/DynamicSystem.cpp", "src/include/fparser/fparser.cc", "src/include/fparser/fpoptimizer.cc"],
+        include_dirs=include_dirs,
         # Example: passing in the version to the compiled code
         define_macros = [('VERSION_INFO', __version__)],
         ),
 ]
 
 setup(
-    name="python_example",
+    name="dyns",
     version=__version__,
-    author="Sylvain Corlay",
-    author_email="sylvain.corlay@gmail.com",
-    url="https://github.com/pybind/python_example",
-    description="A test project using pybind11",
+    author="Karachurin Raul, Ladygin Stanislav",
+    author_email="karrauln@yandex.ru",
+    url="https://github.com/FishermenOnTuesdays/pydyns",
+    description="A test project for Python bindings for DynS C++ library",
     long_description="",
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
